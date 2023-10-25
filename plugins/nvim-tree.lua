cfb44feb2163a76c7d0df05e1e1c3be397ce046f
nanashi-1 <bagaresalken@gmail.com>
2023-10-25 21:33:49 +0000

    change keymap to match ranger

--- plugins/nvim-tree.lua
+++ plugins/nvim-tree.lua
@@ -5,11 +5,26 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
--- OR setup with some options
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', 'l',    api.node.open.edit,                  opts('Open'))
+  vim.keymap.set('n', 'h',    api.node.navigate.parent_close,      opts('Close Directory'))
+end
+
 require("nvim-tree").setup({
+  on_attach = on_attach,
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 30
   },
   renderer = {
     group_empty = true,
